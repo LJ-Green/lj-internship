e8b11bbb3c4d72dabc91ace50c65f63ac7a193a4
LJ <ljdgreen@live.co.uk>
2023-06-13 20:43:57 +0100

    Dynamic Hot Collections Feature

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -11,17 +9,13 @@ import "./styles.css";
 
 const HotCollections = () => {
   const [collections, setCollections] = useState([]);
-  const [slidesPerView, setSlidesPerView] = useState(2);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
-        );
+        const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
         setCollections(response.data);
-        console.log(response.data);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching hot collections:", error);
@@ -30,98 +24,96 @@ const HotCollections = () => {
 
     const timer = setTimeout(() => {
       fetchData();
-    }, 2000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 576) {
-        setSlidesPerView(1);
-      } else if (window.innerWidth < 768) {
-        setSlidesPerView(2);
-      } else {
-        setSlidesPerView(4);
-      }
-    };
+  const breakpoints = {
+    390: {
+      slidesPerView: 1
+    },
+    660: {
+      slidesPerView: 2
+    },
+    856: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 4
+    }
+  };
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  const collectionItems = collections.map((collection, index) => (
+    <SwiperSlide key={index}>
+      <div className="nft_coll">
+        <div className="nft_wrap">
+          <Link to={`/item-details/${collection.authorId}`}>
+            <img
+              src={collection.nftImage}
+              className="lazy img-fluid"
+              alt=""
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Link>
+        </div>
+        <div className="nft_coll_pp">
+          <Link to="/author">
+            <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+          </Link>
+          <i className="fa fa-check"></i>
+        </div>
+        <div className="nft_coll_info">
+          <Link to="/explore">
+            <h4>{collection.title}</h4>
+          </Link>
+          <span>ERC-{collection.code}</span>
+        </div>
+      </div>
+    </SwiperSlide>
+  ));
 
   return (
-    <>
-      <section id="section-collections" className="no-bottom">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="text-center">
-                <h2>Hot Collections</h2>
-                <div className="small-border bg-color-2"></div>
-              </div>
+    <section id="section-collections" className="no-bottom">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Hot Collections</h2>
+              <div className="small-border bg-color-2"></div>
             </div>
-            {loading ? (
-              <div className="skeleton-container">
-                {[...Array(4)].map((_, index) => (
-                  <div className="skeleton-item" key={index}></div>
-                ))}
-              </div>
-            ) : (
-            <Swiper
-              navigation={true}
-              modules={[Navigation]}
-              className="mySwiper"
-              slidesPerView={slidesPerView}
-              spaceBetween={20}
-              pagination={{
-                clickable: true,
-              }}
-              style={{
-                "--swiper-navigation-size": "20px",
-              }}
-            >
-              {collections.map((collection, index) => (
-                <SwiperSlide key={index}>
-                  <div className="" key={index}>
-                    <div className="nft_coll">
-                      <div className="nft_wrap">
-                        <Link to={`/item-details/${collection.authorId}`}>
-                          <img
-                            src={collection.nftImage}
-                            className="lazy img-fluid"
-                            alt=""
-                          />
-                        </Link>
-                      </div>
-                      <div className="nft_coll_pp">
-                        <Link to="/author">
-                          <img
-                            className="lazy pp-coll"
-                            src={collection.authorImage}
-                            alt=""
-                          />
-                        </Link>
-                        <i className="fa fa-check"></i>
-                      </div>
-                      <div className="nft_coll_info">
-                        <Link to="/explore">
-                          <h4>{collection.title}</h4>
-                        </Link>
-                        <span>{collection.authorId}</span>
-                      </div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-            )}
           </div>
+          {loading && !collections.length ? (
+            <div className="skeleton-container">
+              {[...Array(4)].map((_, index) => (
+                <div className="skeleton-item" key={index}></div>
+              ))}
+            </div>
+          ) : (
+            collections.length > 0 ? (
+              <Swiper
+                navigation
+                modules={[Navigation]}
+                className="mySwiper"
+                breakpoints={breakpoints}
+                loop
+                spaceBetween={20}
+                pagination={{
+                  clickable: true
+                }}
+                style={{
+                  "--swiper-navigation-size": "15px"
+                }}
+              >
+                {collectionItems}
+              </Swiper>
+            ) : (
+              <div>No collections available</div>
+            )
+          )}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
